739c4d9b4b8d0335d311588cf65363174d2c27b3
Bum-Ho12 <bumhonisu@gmail.com>
2022-11-29 09:07:49 +0300

    upgrade face detection
    minimized errors by cropping out faces

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from kivy.graphics.texture import Texture
 from kivy.clock import Clock
 import cv2
 import numpy as np
-import tensorflow as tf
+import keras
+import sys
 
+cascPath = sys.argv[0]
 class MainApp(App):
     def build(self):
-        self.model = tf.keras.models.load_model('DreeNGzz.model')
+        
+        self.faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.model = keras.models.load_model('DreeNGzz.model')
         window = BoxLayout(orientation = 'vertical')
         self.image = Image()
         window.add_widget(self.image)
@@ -37,7 +41,7 @@ class MainApp(App):
         Clock.schedule_interval(self.load_video,1.0/30.0)# apply 30fps
         return window
     def load_video(self,*args):
-        ret, frame = self.capture.read()
+        ret, frame = self.capture.read()# reading one frame
         self.image_frame = frame
         buffer = cv2.flip(frame,0).tobytes()
         texture = Texture.create(size = (frame.shape[1],frame.shape[0]),
@@ -47,14 +51,27 @@ class MainApp(App):
         #and buffer format in rapid format in real time
         self.image.texture = texture
     def save_image(self,*args):
-        cv2.imwrite('image.jpg', self.image_frame) 
+        cv2.imwrite('image.jpg', self.image_frame)
+        im = cv2.imread('image.jpg')
+        # Convert into grayscale
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = self.faceCascade.detectMultiScale(gray, 1.1, 4)
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(im, (x, y), (x+w, y+h), 
+                        (0, 0, 255), 2)
+            
+            faces = im[y:y + h, x:x + w]
+            # cv2.imshow("face",faces)
+            cv2.imwrite('face.jpg', faces)
 
         def load_image(file_path):
             image_array = cv2.imread(file_path,cv2.IMREAD_GRAYSCALE)
             new_array = cv2.resize(image_array,(128,128))
+            # cv2.imwrite('image1.jpg', new_array)
             return new_array.reshape(1,128,128,1)
 
-        prediction = self.model.predict([load_image('image.jpg')])
+        prediction = self.model.predict([load_image('face.jpg')])
 
         sex=int(np.round(prediction))
         GENDER = ['Male','Female']
